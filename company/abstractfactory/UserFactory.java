0508d6ee3f6f7acba1054d41a54eb54f3b2b9bc6
therealjuuf <96357263+therealjuuf@users.noreply.github.com>
2022-03-09 09:03:14 +0300

    Delete company directory

--- company/abstractfactory/UserFactory.java
+++
@@ -1,7 +0,0 @@
-package com.company.abstractfactory;
-import com.company.abstractfactory.UserX;
-public class UserFactory {
-    public static UserX getUser(UserAbstractFactory factory){
-        return factory.createUser();
-    }
-}
--- company/adapter/GradesAdapter.java
+++
@@ -1,5 +0,0 @@
-package com.company.adapter;
-
-public interface GradesAdapter {
-    String getGrade();
-}
